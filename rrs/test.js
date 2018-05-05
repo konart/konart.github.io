742a81c6463b9d03c5d3abb5e32eaf1c8440e703
Артем Кондратьев <konart@iMac-Mac.lan>
2018-05-05 22:40:16 +0300

    misprint

--- rrs/test.js
+++ rrs/test.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 
-var body = 'title=mememe%id=3'
+var body = 'title=mememe&id=3'
 
 xhr.open("POST", 'https://t0zsgw77r5.execute-api.eu-west-1.amazonaws.com/beta/rrs', true);
 xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
