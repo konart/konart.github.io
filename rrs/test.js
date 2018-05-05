5bdfcb99a78f34a60a7fe54323f74aa224c6a88f
Артем Кондратьев <konart@iMac-Mac.lan>
2018-05-05 22:33:59 +0300

    Added CORS

--- rrs/test.js
+++ rrs/test.js
@@ -2,8 +2,9 @@ var xhr = new XMLHttpRequest();
 
 var body = 'title=mememe%id=3'
 
-xhr.open("POST", 'https://t0zsgw77r5.execute-api.eu-west-1.amazonaws.com/beta/rrs/entry/3', true);
+xhr.open("POST", 'https://t0zsgw77r5.execute-api.eu-west-1.amazonaws.com/beta/rrs', true);
 xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+xhr.setRequestHeader('Origin', 'https://konart.github.io');
 
 xhr.onreadystatechange = function () {
     if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
