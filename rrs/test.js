3a9c2dda98732e2d1e7d52990588d6778ccbf60e
Артем Кондратьев <konart@iMac-Mac.local>
2018-05-08 23:24:11 +0300

    static -> dynamic

--- rrs/test.js
+++ rrs/test.js
@@ -1,20 +1,18 @@
 var xhr = new XMLHttpRequest();
+var data = "";
 
-var body = 'title=mememe&id=3'
-
-// xhr.open("GET", 'https://ig0wxl2wy3.execute-api.eu-west-1.amazonaws.com/beta/items', true);
+xhr.open("GET", 'https://ig0wxl2wy3.execute-api.eu-west-1.amazonaws.com/beta/items', true);
 // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-// xhr.setRequestHeader('Origin', 'https://konart.github.io');
 
-// xhr.onreadystatechange = function () {
-//     if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-//         alert(xhr.responseText);
-//     };
-// };
+xhr.onreadystatechange = function () {
+    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        data = JSON.parse(xhr.responseText)
+    };
+};
 
-// xhr.send(body);
+xhr.send(body);
 
-var data = JSON.parse('{"id": "1", "url": "http://ya.ru", "title": "Mega title", "date": "Today", "itemtype": "test"}')
+// var data = JSON.parse('{"id": "1", "url": "http://ya.ru", "title": "Mega title", "date": "Today", "itemtype": "test"}')
 
 itemsbar = document.getElementById("items")
 
