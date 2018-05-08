b8b702ac8bd77b27b5a1a02398690869f00d98f2
Артем Кондратьев <konart@iMac-Mac.local>
2018-05-08 22:55:33 +0300

    use fake data for now

--- rrs/test.js
+++ rrs/test.js
@@ -2,17 +2,27 @@ var xhr = new XMLHttpRequest();
 
 var body = 'title=mememe&id=3'
 
-xhr.open("POST", 'https://t0zsgw77r5.execute-api.eu-west-1.amazonaws.com/beta/rrs', true);
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-xhr.setRequestHeader('Origin', 'https://konart.github.io');
+// xhr.open("GET", 'https://ig0wxl2wy3.execute-api.eu-west-1.amazonaws.com/beta/items', true);
+// xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+// xhr.setRequestHeader('Origin', 'https://konart.github.io');
 
-xhr.onreadystatechange = function () {
-    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        alert(xhr.responseText);
-    };
-};
+// xhr.onreadystatechange = function () {
+//     if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+//         alert(xhr.responseText);
+//     };
+// };
 
-xhr.send(body);
+// xhr.send(body);
+
+var data = JSON.parse('{"id": "1", "url": "http://ya.ru", "title": "Mega title", "date": "Today", "itemtype": "test"}')
+
+itemsbar = document.getElementById("items")
+
+var item = document.createElement('div');
+item.className = "content c"
+item.innerHTML = '<div class="title"><a href="' + data.url + '">' + data.title +'</div><div class="done action"></div><div class="renew action">R</div>'
+
+itemsbar.appendChild(item)
 
 els = document.getElementsByClassName("action")
 for (var i = 0; i < els.length; i++) {
@@ -22,4 +32,3 @@ for (var i = 0; i < els.length; i++) {
         console.log(event.target.textContent + "!");
     }, false);
 };
-
